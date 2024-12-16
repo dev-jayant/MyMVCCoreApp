20698ddde9b43d6310711da439811159a2839266
jayantmandal <jayantgiddi@gmail.com>
2024-12-16 10:44:19 +0530

    Add project files.

---
+++ MyMVCCoreApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyMVCCoreApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
